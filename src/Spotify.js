56e10003c829bebf3bd100236a81029621443ab3
Charles S <charlessoaresdossantos@gmail.com>
2019-01-26 01:12:41 -0200

    refactored and created the components

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQDASpa-9o37dQx3beuyCmosKs8u1FqPKxxzEMMaPj4cv0OpS_A_nhWwth5Us-_S-yAZymekUX23k5YMQI3LdKKnlxEMyF4wjq6y3UU5Uz5rPJuyL6R4lGqoFqK1XcpTQL5fEq1OAGQ2baMyWoc0yWwn108GwW76zid1i34'
+  token: 'BQAJZDJ4BPRF2st_owpywi3mVcoJy_T_DGM_owz39p97tO39Qz_VbEjwsuABCNE3hD4NstFJHgwrWZ3pG8-q7RWLGwxRRrRpxDm_EhIwXYSQ8HxXJaF6-f6pvYnFBD4ouIksx0fd3NwMbn-oLW5k4Rn-HKsct5rKZMMv960'
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log('Spotify player');
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
+
+//do call to API
+const albums = spotify.search.albums('Incubu');
+const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
+
+//get the elements to append the inner elements
+const albumList = document.getElementById('album-list');
+const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-tracks');
+
+albums
+    .then(data => renderAlbums(data.albums.items, albumList));
+
+album
+    .then(data => renderAlbumInfo(data, albumInfo))
+    .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
